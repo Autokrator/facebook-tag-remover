68cbea99c35f8d4635e99761e89b19e818ebbc6a
Nelson Ong <contact@nelsonong.com>
2015-09-12 12:55:32 -0400

    Loop tag-removal every 1s
    This is done in case more DOM elements are added later on (ex. User clicks
    on 'View more comments')

--- contentscript.js
+++ contentscript.js
@@ -1,2 +1,6 @@
-$('li.UFIComment:has(a[href$="hc_location=ufi"])').next().filter('.UFIReplyList').remove();
-$('li.UFIComment:has(a[href$="hc_location=ufi"])').remove();
+function removeTagComments() {
+	$('li.UFIComment:has(a[href$="hc_location=ufi"])').next().filter('.UFIReplyList').remove();
+	$('li.UFIComment:has(a[href$="hc_location=ufi"])').remove();
+}
+
+setInterval(removeTagComments, 1000);
